4f62393ef364d7bfcdf8f768d5cdfab66725bf46
Phyllis Wong <wong.phyllism@gmail.com>
2019-06-11 16:33:32 -0700

    getting JSON back from the endpoint

--- example/example/Networking.swift
+++ example/example/Networking.swift
@@ -7,7 +7,113 @@
 //
 
 import Foundation
+import Alamofire
+import PromiseKit
 
+typealias JSON = [String : Any]
+
+// enum used for convenience purposes (optional)
+public enum ServerURL: String {
+  case base = "https://participants-stg.evolv.ai"
+  
+  // additional endpoints
+}
+
+//public struct RestClient {
+//  let environment_id = "5eadef5e68"
+//  var baseURL: String
+
+  // FIXME: this is failable, change the force unwrapping later
+//  let url = URL(string: "https://participants-stg.evolv.ai/v1/5eadef5e68/configuration")!
+//  let session = URLSession.shared
+//  
+//  let task = session.dataTask(with: url, completionHandler: { data, response, error in
+//    // do something with the stuff
+//    print(data)
+//    print(response)
+//    print(error)
+//  })
+//  
+//  task.resume()
+//
+//  var defaultHeader: HTTPHeaders = [
+//    "Content-Type" : "application/json"
+//  ]
+//
+//  public init(baseURL: ServerURL = .base) {
+//    self.baseURL = baseURL.rawValue
+//  }
+  
+//}
+
+public enum Resource {
+  case getConfig
+  
+  public var resource: (method: HTTPMethod, route: String) {
+    let environment_id = "5eadef5e68"
+    switch self {
+    case .getConfig:
+      return (.get, "/v1/\(environment_id)/configuration")
+    }
+  }
+}
+
+//class networking {
+//  let configuration = URLSessionConfiguration.ephemeral
+//  let session = URLSession(configuration: configuration, delegate: nil, delegateQueue: OperationQueue.main)
+//  let environment_id = "5eadef5e68"
+//  let url: String = "https://participants-stg.evolv.ai/v1/\(environment_id)/configuration"
+//
+//
+//  let task = session.dataTask(with: url, completionHandler: { [weak self] (data: Data?, response: URLResponse?, error: Error?) -> Void in
+//    // Parse the data in the response and use it
+//  })
+//  task.resume()
+//
+//}
+
+//  public func request(_ resource: Resource,
+//                      parameters: [String : Any] = [:]
+//    headers: HTTPHeaders = [:]) -> Promise < JSON > {
+//    return Promise { fulfill, reject in
+//      let method = resource.resource.method
+//      let url = "\(baseURL)\(resource.resource.route)"
+//      var header = defaultHeader
+//
+//      /*
+//       prepare additional stuff like:
+//       - more header params
+//       - edit/change the endpoint when something specifically is called
+//       - creae a signature for security purposes\
+//       ect.
+//       */
+//
+//      Alamofire.request(u, method: method,
+//                        parameters: method == .get ? nil : params,
+//                        encoding: JSONEncoding.default,
+//                        headers: headers).responseJSON { (response) in
+//
+//                          switch response.result {
+//                            case .success(let json):
+//                              // If there is not JSON data, cause an error ('reject' function)
+//                              guard let json = json as? JSON else {
+//                                return reject(AFError.responseValidationFailed(reason: .dataFileNil))
+//                              }
+//                              // pass the JSON data into the fulfill function, so we can receive the value
+//                              fulfill(json)
+//                            case .failure(let error):
+//                              // pass the error into the reject function, so we can check what causes the error
+//                              reject(error)
+//                          }
+//      }
+//
+//    }
+//  }
+//
+//}
+
+
+// FIXME: is this needed with Alomofire and PromiseKit?
 //extension URLSession {
 //  func request(url: URL) -> Future<Data> {
 //    // Start by constructing a Promise, that will later be
--- example/example/ViewController.swift
+++ example/example/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  enum DataError: Error {
+    case taskError
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    let url = URL(string: "https://participants-stg.evolv.ai/v1/5eadef5e68/configuration")!
+    let session = URLSession.shared
+    
+    let task = session.dataTask(with: url, completionHandler: { data, response, error in
+      // do something with the stuff
+      
+      // 1 check for errors
+      if error != nil {
+        // OH NO! An error occured ...
+        // self.handleClientError(error) // create a function that can deal with the error
+        print(error ?? "SOMETHING REALLY BAD HAPPEND!")
+      }
+      
+      // 2 check the status code
+      guard let httpResponse = response as? HTTPURLResponse,
+        (200...299).contains(httpResponse.statusCode) else {
+          // self.handleServerError(response)
+          print(response ?? "SERVER SENT BACK A BAD STATUS CODE")
+          return
+      }
+      
+      // 3 check that we got back JSON
+      guard let mime = response?.mimeType, mime == "application/json" else {
+        print("Wrong MIME type!!!!!!")
+        return
+      }
+      
+      // 4 check what the data was that came back
+      if let json = try? JSONSerialization.jsonObject(with: data!, options: []) {
+        print(json)
+      }
+    })
+    
+    task.resume()
   }
-
-
 }
 
