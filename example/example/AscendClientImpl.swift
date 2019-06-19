f15db39079b7c17455bc61bc7cfb3911599969b9
Phyllis Wong <wong.phyllism@gmail.com>
2019-06-18 17:16:08 -0700

    start the ascend client

--- example/example/AscendClientImpl.swift
+++ example/example/AscendClientImpl.swift
@@ -8,9 +8,24 @@
 
 import Foundation
 
-class AscendClientImpl : AscendClient {
-  typealias ActionType = T
+class AscendClientImpl { // : AscendClient {
+  
+  private let logger = Log.logger
+  private let allocator: Allocator
+  private let store: AscendAllocationStore
+  private let previousAllocations: Bool
+  private let participant: AscendParticipant
+  
+  init(config: AscendConfig, allocator: Allocator, previousAllocations: Bool, participant: AscendParticipant) {
+    self.store = config.getAscendAllocationStore()
+    self.allocator = allocator
+    self.previousAllocations = previousAllocations
+    self.participant = participant
+  }
   
   
+  func get<T>(key: String, defaultValue: T) {
+    
+  }
   
 }
