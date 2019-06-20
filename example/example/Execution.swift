942859cc34a3c8c14ea1cdfb9616af432d6682fb
Phyllis Wong <wong.phyllism@gmail.com>
2019-06-19 18:00:02 -0700

    wip

---
+++ example/example/Execution.swift
@@ -0,0 +1,62 @@
+//
+//  Execution.swift
+//  example
+//
+//  Created by phyllis.wong on 6/19/19.
+//  Copyright © 2019 phyllis.wong. All rights reserved.
+//
+
+import Foundation
+
+// TODO: move this to own file
+protocol AscendAction {
+  /**
+   * Applies a given value to a set of instructions.
+   * @param value any value that was requested
+   */
+  func apply<T>(value: T) -> Void
+}
+
+public class GenericClass<T> {
+  private let type: AnyClass
+  
+  public class GenericClass(type: Class<T>) {
+    self.type = type
+  }
+  
+  public func getMyType() -> AnyClass {
+    return self.type
+  }
+}
+
+struct Set<Element> where Element : Hashable {}
+
+class Execution {
+  
+  private let key: String
+  private let defaultValue: Any // Generic
+  private let function: AscendAction
+  private let participant: AscendParticipant
+  
+  private var alreadyExecuted: Set<String> = Set()
+  
+  init(key: String, defaultValue: Any,
+       function: AscendAction,
+       participant: AscendParticipant) {
+    self.key = key
+    self.defaultValue = defaultValue
+    self.function = function
+    self.participant = participant
+  }
+  
+  func getKey() -> String { return self.key }
+  
+  func executeWithAllocation(rawAllocations: JsonArray) throws {
+    
+  }
+  
+  func executeWithDefault() {
+    self.function.apply(value: self.defaultValue)
+  }
+  
+}
