3e58e88aaa4b41bfba5964eef1e941dd81b0ef54
Phyllis Wong <wong.phyllism@gmail.com>
2019-06-21 16:58:38 -0700

    cache methods work, can't test it because the cache is cleared at runtime

--- example/example/ViewController.swift
+++ example/example/ViewController.swift
@@ -33,7 +33,8 @@ private extension ViewController {
     let httpClient = HttpClient()
     let envId = "40ebcd9abf"
     let config = ConfigBuilder(environmentId: envId).buildConfig()
-    let alloc = Allocator(config: config, participant: participant, httpClient: httpClient)
+    let store = LRUCache<String>(10)
+    let alloc = Allocator(store: store, config: config, participant: participant, httpClient: httpClient)
     let results = alloc.fetchAllocations()
     print("YOUR FETCHED ALLOCATION: \(String(describing: results))")
   }
@@ -45,8 +46,9 @@ private extension ViewController {
     let httpClient = HttpClient()
     let envId = "40ebcd9abf"
     let config = ConfigBuilder(environmentId: envId).buildConfig()
-    let alloc = Allocator(config: config, participant: participant, httpClient: httpClient)
-    let futureAloc = [alloc.fetchAllocations()]
+    let store = LRUCache<String>(10)
+    let alloc = Allocator(store: store, config: config, participant: participant, httpClient: httpClient)
+    let futureAloc = alloc.fetchAllocations()
     let emitter = EventEmitter(httpClient: httpClient, config: config, participant: participant)
     let ascender = AscendClientImpl(config: config, allocator: alloc, previousAllocations: false, participant: participant, eventEmitter: emitter, futureAllocations: futureAloc)
     let value = ascender.get(key: "button", defaultValue: "green")
